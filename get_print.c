b69b2928168dceb03f7301ee0a03837554bc8061
chitaaaaa <95076075+chitaaaaa@users.noreply.github.com>
2022-04-20 07:47:44 +0900

    Update get_print.c

--- get_print.c
+++ get_print.c
@@ -16,7 +16,18 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'i', print_int},
 		{'s', print_string},
 		{'c', print_char},
-			};
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
 	int flags = 14;
 
 	register int i;
