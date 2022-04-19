899c7368a19c693ccbbae5ad974f9c9ee51e60f1
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-04-19 13:13:17 -0700

    commit

--- main.h
+++ main.h
@@ -1,33 +1,36 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-int _printf(const char *format, ...);
-int _position(const char *s, int n);
-int _strlen(char *s);
-char *_strcat(char *dest, char *src, int n);
-int _abs(int n);
-int _numlen(int n);
-void *rev_string(char *s);
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * struct type - Struct data type
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
  *
- * @op: data type argument
- * @f: The function associated
+ * Return: int
  */
-
-typedef struct type
+typedef struct structprint
 {
-	char *op;
-	char *(*f)(va_list);
-} type_t;
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
 
-char *print_c(va_list list);
-char *print_s(va_list list);
-char *print_i(va_list list);
-char *print_bin(va_list list);
-#endif 
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
+#endif
