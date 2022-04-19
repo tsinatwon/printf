269b1273f822764766555c9b7ef48c37dbef3d24
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-04-19 14:42:09 -0700

    group work

---
+++ print_bases.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_hex - prints a number in hexadecimal base,
+ * in lowercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_hex(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 1);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0x");
+	count += _puts(str);
+	return (count);
+}
